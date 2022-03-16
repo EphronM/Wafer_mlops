4d7980accd4853287cf33628168a48054b44c1ab
Ephron Martin <ephronmartin2016@gmail.com>
2022-03-16 14:19:20 +0530

    'read_config

--- src/pipeline_01_data_preparation.py
+++ src/pipeline_01_data_preparation.py
@@ -1,11 +1,18 @@
 from email.policy import default
 import os
 import argparse
+from numpy import DataSource
 import yaml
 import logging 
 
+def read_params(path):
+    with open (path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
 
-
+def main(config_path, datasource):
+    config = read_params(config_path)
+    print (config)
 
 
 
@@ -14,9 +21,10 @@ import logging
 
 if __name__=='__main__':
     args = argparse.ArgumentParser()
-    args.add_argument('--config', default= default)
+    default_config = os.path.join("config", "params.yaml")
+    args.add_argument('--config', default= default_config)
     args.add_argument('--datasource', default= None)
 
     parsed_args = args.parse_args()
-    print(parsed_args.config)
-    print(parsed_args.datasource)
+    main(config_path = parsed_args.config, datasource=parsed_args.datasource)
+  
